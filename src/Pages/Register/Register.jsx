cb95d0f3b18a4a6ad0e8b8eea1b326781ca1c6b4
tanvir <tanvir0046@gmail.com>
2023-11-17 22:55:17 +0600

    login and register section is done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,6 +2,14 @@ import background from '../../assets/others/authentication.png'
 import loginImage from '../../assets/others/authentication2.png'
 
 const Register = () => {
+    const handleRegister = e => {
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+    }
+
     return (
         <div className="w-full h-[100vh] flex justify-center items-center" style={{ background: `url(${background})` }}>
             <div className=" w-4/5 h-3/4 shadow-2xl" style={{ background: `url(${background})` }}>
@@ -10,25 +18,25 @@ const Register = () => {
                         <img className='w-[80%]' src={loginImage} alt="" />
                     </div>
                     <div className="card shrink-0 w-full max-w-sm">
-                        <form className="card-body">
+                        <form onSubmit={handleRegister} className="card-body">
                             <div className="form-control">
                                 <h1 className="text-2xl font-bold text-center">Register</h1>
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <input className='btn bg-[#D1A054B3] text-white' type="submit" value="Submit" />
                             </div>
                         </form>
                     </div>
