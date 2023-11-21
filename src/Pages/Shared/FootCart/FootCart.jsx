145ee25f3e05394efc0619fedb7c35d1b7cd8e0b
tanvir <tanvir0046@gmail.com>
2023-11-21 22:38:14 +0600

    webside dark mode toggle is working now

--- src/Pages/Shared/FootCart/FootCart.jsx
+++ src/Pages/Shared/FootCart/FootCart.jsx
@@ -1,6 +1,31 @@
-
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../../Hooks/useAuth";
+import Swal from 'sweetalert2'
 
 const FootCart = ({ item }) => {
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const handleAddItem = food => {
+        console.log(food)
+        if (user && user.email) {
+            // todo: do something
+        } else {
+            Swal.fire({
+                title: "You are not Logged In !",
+                text: "Please login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    //send to the login page
+                    navigate('/login')
+                }
+            });
+        }
+    }
     return (
         <div key={item._id} className="card bg-base-100 shadow-xl rounded-none">
             <figure><img className="w-full" src={item.image} alt="Shoes" /></figure>
@@ -11,7 +36,9 @@ const FootCart = ({ item }) => {
                 <h2 className="font-bold text-xl">{item.name}</h2>
                 <p>{item.recipe}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn text-[#BB8506] border-b-4 border-[#BB8506] border-t-0 border-l-0 border-r-0 hover:bg-[#1F2937] hover:border-none">Add To Cart</button>
+                    <button
+                        onClick={() => handleAddItem(item)}
+                        className="btn text-[#BB8506] border-b-4 border-[#BB8506] border-t-0 border-l-0 border-r-0 hover:bg-[#1F2937] hover:border-none">Add To Cart</button>
                 </div>
             </div>
         </div>
