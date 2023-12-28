2ccf5dc62e6254ca46330f8096c5b135f9e66c4d
tanvir <tanvir0046@gmail.com>
2023-12-28 22:43:42 +0600

    today work done

--- src/Hooks/useMenu.jsx
+++ src/Hooks/useMenu.jsx
@@ -1,16 +1,31 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+import { useQuery } from "@tanstack/react-query"
+import useAxiosPublic from "./useAxiosPublic"
+
 const useMenu = () => {
-    const [menus, setMenus] = useState([])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        fetch('http://localhost:3100/menu')
-            .then(res => res.json())
-            .then(data => {
-                setMenus(data)
-                setLoading(false)  
-            })
-    }, [])
-    return [menus, loading]
+    const axiosPublic = useAxiosPublic();
+    // const [menus, setMenus] = useState([])
+    // const [loading, setLoading] = useState(true)
+    // useEffect(() => {
+    //     fetch('http://localhost:3100/menu')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setMenus(data)
+    //             setLoading(false)  
+    //         })
+    // }, [])
+
+    const { data: menu = [], isPending: loading, refetch } = useQuery({
+        queryKey: ['menu'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/menu');
+            return res.data;
+        }
+    })
+
+
+    return [menu, loading, refetch]
 }
 
 export default useMenu
--- src/Pages/DashBoard/ManageItems/ManageItems.jsx
+++ src/Pages/DashBoard/ManageItems/ManageItems.jsx
@@ -1,11 +1,60 @@
-import { FaUsers } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 import SectionTitle from "../../../Components/SectionTital/SectionTitle";
 import useMenu from "../../../Hooks/useMenu";
 import { RiDeleteBinLine } from "react-icons/ri";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 
 const ManageItems = () => {
     const [menu] = useMenu();
+    const axiosSecure = useAxiosSecure();
+
+    const handleDeleteItem = (item) => {
+        console.log(item._id)
+        // Swal.fire({
+        //     title: "Are you sure?",
+        //     text: "You won't be able to revert this!",
+        //     icon: "warning",
+        //     showCancelButton: true,
+        //     confirmButtonColor: "#3085d6",
+        //     cancelButtonColor: "#d33",
+        //     confirmButtonText: "Yes, delete it!"
+        // }).then(async (result) => {
+        //     if (result.isConfirmed) {
+        //         const res = await axiosSecure.delete(`/menu/${item._id}`)
+        //         console.log(res.data)
+
+        //         // if (res.data.deletedCount > 0) {
+        //         //     Swal.fire({
+        //         //         title: "Deleted!",
+        //         //         text: `has been deleted.`,
+        //         //         icon: "success"
+        //         //     });
+        //         // }
+        //     }
+        // });
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/menu/${item._id}`)
+                console.log(res.data)
+                // Swal.fire({
+                //     title: "Deleted!",
+                //     text: "Your file has been deleted.",
+                //     icon: "success"
+                // });
+            }
+        });
+    }
+
     return (
         <div>
             <SectionTitle
@@ -14,7 +63,7 @@ const ManageItems = () => {
             <div>
                 <div className="bg-white mx-auto p-4">
                     <div className="flex justify-between">
-                        <h2 className="text-3xl">Items: {menu.length}</h2>
+                        <h2 className="text-3xl mb-5">Items: {menu.length}</h2>
                         {/* <h2 className="text-3xl">Total Price: {totalPrice}</h2> */}
                     </div>
                     <div className="overflow-x-auto">
@@ -39,15 +88,13 @@ const ManageItems = () => {
                                         <td>{user.name}</td>
                                         <td>{user.category}</td>
                                         <td>
-                                            {
-                                                user.role === 'admin' ? 'Admin' : <button onClick={() => handleMakeAdmin(user)} className="btn bg-orange-500 text-white">
-                                                    <FaUsers />
-                                                </button>
-                                            }
+                                            <button onClick={() => handleUpdate(item)} className="btn bg-orange-500 text-white">
+                                                <FaEdit />
+                                            </button>
                                         </td>
                                         <td>
                                             <button>
-                                                <button onClick={() => handleDeleteUser(user)} className="btn bg-[#B91C1C] w-10 h-10 flex items-center justify-center btn-xs"><RiDeleteBinLine
+                                                <button onClick={() => handleDeleteItem(user)} className="btn bg-[#B91C1C] w-10 h-10 flex items-center justify-center btn-xs"><RiDeleteBinLine
                                                     className="text-2xl text-white" /></button>
                                             </button>
                                         </td>
